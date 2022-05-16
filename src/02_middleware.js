0e363218871ec2bbba9affc3ad2ff92ed92a308c
EliotCao <484900039@qq.com>
2022-05-16 20:40:21 +0800

    Add middleware

---
+++ src/02_middleware.js
@@ -0,0 +1,21 @@
+//一.导入koa包
+const Koa = require('koa')
+//二.实例化app对象
+const app = new Koa()
+//三.编写中间件
+app.use((ctx, next) => {
+    console.log('我来组成头部')
+    next()
+})
+app.use((ctx, next) => {
+    console.log('我来组成身体')
+    next()
+})
+app.use((ctx, next) => {
+    console.log('组装完成')
+    ctx.body = '组装完成'
+})
+//四.启动服务，监听3000端口
+app.listen(8080, () => {
+    console.log('server is running on http://localhost:8080')
+})
