5b16a45ad85387813728eaf39a9bd934a94994b0
EliotCao <484900039@qq.com>
2022-05-20 21:09:32 +0800

    Optimize 07_koa-router

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,9 +1,24 @@
 const Router = require('koa-router')
 
 const router = new Router({prefix: '/user'})
+const db = [
+    { id: 1, name: 'xiaoming', age: 20 },
+    { id: 2, name: 'xiaomei', age: 18 },
+    { id: 3, name: 'xiaopang', age: 2 },
+]
 
 router.get('/', (ctx) => {
-    ctx.body = '用户页'
+    ctx.body = db
+})
+// GET /users/:id ---- 根据id获取单个用户的信息, 返回一个对象
+router.get('/:id', (ctx) => {
+    // 解析id参数
+    const id = ctx.params.id
+    const res = db.filter((item) => item.id == id)
+
+    if (!res[0]) ctx.throw(404)
+
+    ctx.body = res[0]
 })
 router.post('/', (ctx) => {
     ctx.body = '创建用户'
